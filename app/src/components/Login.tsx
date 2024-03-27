cc8b76cad05c9beae49647866c8169610a2ae19a
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-27 23:29:25 +0100

    feat(components): navigate user to Home page after Login

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { signInWithGoogle } from '../hooks/useAuth';
 
 export default function Login() {
+    const navigate = useNavigate();
+
     return (
         <div className="mx-auto text-center mb-24">
             <button type="button" className="text-white bg-blue-500 focus:ring-2 focus:outline-none focus:ring-blue-300 rounded-xl text-sm mx-auto px-7 py-3"
                 onClick={() => signInWithGoogle()
-                    .then(user => console.log('Logged in user:', user))
+                    .then(user => {
+                        console.log('Logged in user:', user);
+                        navigate('/home');
+                    })
                     .catch(error => console.error("Login failed: ", error))}>
                 Sign in with Google
             </button>
