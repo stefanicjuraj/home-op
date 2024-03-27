5b6aa70e8d91bd2c66a3407f4be12f0a48bfdca1
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-27 11:54:44 +0100

    feat(utils): add formatDate

---
+++ app/src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+export const formatDateInput = (date: Date | { seconds: number }): string => {
+  if (!date) return "";
+
+  const jsDate = date instanceof Date ? date : new Date(date.seconds * 1000);
+
+  const year = jsDate.getFullYear();
+  const month = jsDate.getMonth() + 1;
+  const day = jsDate.getDate();
+
+  const formattedMonth = month < 10 ? `0${month}` : month;
+  const formattedDay = day < 10 ? `0${day}` : day;
+
+  return `${year}-${formattedMonth}-${formattedDay}`;
+};
