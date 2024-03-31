a2ec1be9e01c3a8fa44fe4dadb3cbb1106c55e40
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-01 00:22:07 +0200

    fix(utils): modify formatDateInput return format

--- app/src/utils/formatDate.ts
+++ app/src/utils/formatDate.ts
@@ -10,5 +10,5 @@ export const formatDateInput = (date: Date | { seconds: number }): string => {
   const formattedMonth = month < 10 ? `0${month}` : month;
   const formattedDay = day < 10 ? `0${day}` : day;
 
-  return `${formattedDay}/${formattedMonth}/${year}`;
+  return `${year}-${formattedMonth}-${formattedDay}`;
 };
