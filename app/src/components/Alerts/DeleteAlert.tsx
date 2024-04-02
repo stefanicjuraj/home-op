26d75e19a133c2b6aa8383ce9da89968ef4684c0
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-02 22:43:08 +0200

    feat(components): add DeleteAlert component

---
+++ app/src/components/Alerts/DeleteAlert.tsx
@@ -0,0 +1,9 @@
+export default function DeleteAlert() {
+    return (
+        <div className="absolute top-0 left-0 w-full flex justify-center z-50">
+            <div className="bg-green-100 border border-green-400 text-gray-900 px-4 py-3 rounded-lg fixed bottom-10">
+                <p>Deleted successfully</p>
+            </div>
+        </div>
+    )
+}
