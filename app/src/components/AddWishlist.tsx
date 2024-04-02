51862ac568b6a1bf19643536eb9b5ac58e93f6ee
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-02 21:20:39 +0200

    style(components): add amount icon to AddWishlist form

--- app/src/components/AddWishlist.tsx
+++ app/src/components/AddWishlist.tsx
@@ -1,6 +1,7 @@
 import { useWishlist } from "../hooks/useWishlist";
 // Icons
 import cost from '/cost.svg';
+import amount from '/amount.svg';
 
 export default function AddWishlist() {
     const { newWishlistItem, setNewWishlistItem, handleInputChange, handleSubmit, formatDateInput } = useWishlist();
@@ -23,15 +24,20 @@ export default function AddWishlist() {
                 </div>
                 <div className="flex-1 min-w-[20%] sm:min-w-[10%] mr-2 my-2">
                     <label htmlFor="amount" className="block mb-1 text-gray-500 text-sm">Amount</label>
-                    <input className="w-full rounded-md border border-gray-500 focus:border-blue-500 p-3"
-                        type="number"
-                        id="amount"
-                        name="amount"
-                        placeholder="Amount"
-                        value={newWishlistItem.amount}
-                        onChange={handleInputChange}
-                        required
-                    />
+                    <div className="relative w-full">
+                        <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3 pointer-events-none">
+                            <img src={amount} className="h-6 w-6" />
+                        </div>
+                        <input className="block p-3.5 w-full z-20 ps-10 text-sm text-gray-900 rounded-md border border-gray-500 focus:border-blue-500 focus:ring-offset-gray-100"
+                            type="number"
+                            id="amount"
+                            name="amount"
+                            placeholder="Amount"
+                            value={newWishlistItem.amount}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
                 </div>
                 <div className="flex-1 min-w-[30%] sm:min-w-[10%] mr-2 my-2">
                     <label htmlFor="cost" className="block mb-1 text-gray-500 text-sm">Cost</label>
